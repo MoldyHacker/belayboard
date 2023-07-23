39c76b69225f89220f52d5e61a13542398dd3baf
moldyhacker <56985918+MoldyHacker@users.noreply.github.com>
2023-07-23 08:55:13 -0500

    added some text to the index page and a route to the new profile page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,16 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+<!--    <img-->
+<!--      alt="Quasar logo"-->
+<!--      src="~assets/quasar-logo-vertical.svg"-->
+<!--      style="width: 200px; height: 200px"-->
+<!--    >-->
+    <div class="text-h3 text-weight-bold text-center absolute-top q-mt-lg">
+      Welcome to BelayBoard!
+      <div class="text-h5 q-mt-lg">
+        The best way to find a climbing partner.
+      </div>
+    </div>
   </q-page>
 </template>
 
---
+++ src/pages/ProfilePage.vue
@@ -0,0 +1,19 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "ProfilePage"
+})
+</script>
+
+<template>
+  <q-page class="">
+    <div class="profileImage text-center q-mt-lg">
+      <q-icon name="account_circle" size="128px"/>
+    </div>
+  </q-page>
+</template>
+
+<style scoped>
+
+</style>
